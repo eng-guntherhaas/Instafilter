9feb6cd3a0426c238c49135de513e320941d1702
guntherhaas <guntherhaas.eng@gmail.com>
2024-07-11 17:03:54 +0200

    initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Gunther Masi Haas on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
